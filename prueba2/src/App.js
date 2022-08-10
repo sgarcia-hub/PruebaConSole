d76b61039933651e50d16b5e8f63342037994c20
ariana2022 <ariana.junco.21@gmail.com>
2022-08-10 16:24:53 -0300

    Update App.js

--- prueba2/src/App.js
+++ prueba2/src/App.js
@@ -1,16 +1,31 @@
+import React from "react";
 import "./App.css";
 import sha256 from "crypto-js/sha256";
 import hmacSHA512 from "crypto-js/hmac-sha512";
 import Base64 from "crypto-js/enc-base64";
 
 function App() {
+  const [ccard, setCcard] = React.useState("");
+  const [ach, setAch] = React.useState("");
+
+  var CryptoJS = require("crypto-js");
+
+  const SubmitsEncry = () => {
+    if (ccard.length == 0 && ach.length == 0) {
+      console.log("Vacio");
+    }
+
+    setCcard([(ccard = CryptoJS.encrypt())]);
+  };
+
   return (
     <>
       <h1>Enter Credit Card Or ACH information</h1>
       <label for="Ccard">Enter Credit Card</label>
-      <input type="text" id="Ccard"></input> <br></br>
+      <input type="text" id="Ccard" onChange={(e) => setCcard()}></input>{" "}
+      <br></br>
       <label for="ACH">Enter ACH information</label>
-      <input type="text" id="ACH"></input> <br></br>
+      <input type="text" id="ACH" onChange={(e) => setAch()}></input> <br></br>
       <button onClick={SubmitsEncry}>Accept</button>
     </>
   );
@@ -18,25 +33,11 @@ function App() {
 
 export default App;
 
-function SubmitsEncry() {
-  console.log("Encrypting...");
+// function SubmitsEncry() {
+//   //console.log("Encrypting...");
 
-  var CryptoJS = require("crypto-js");
+//   var CryptoJS = require("crypto-js");
 
-  var data = "Hello World!";
-  var _secretKey = "some-super-secret-key";
-
-  // var ncryptObject = new ncrypt(_secretKey);
-
-  // // encrypting super sensitive data here
-  // var encryptedData = ncryptObject.encrypt(data);
-  // console.log("Encryption process...");
-  // console.log("Plain Text    : " + data);
-  // console.log("Cipher Text   : " + encryptedData);
-
-  // // decrypted super encrypted string here
-  // var decryptedData = ncryptObject.decrypt(encryptedData);
-  // console.log("... and then decryption...");
-  // console.log("Decipher Text : " + decryptedData);
-  // console.log("...done.");
-}
+//   const encrypted = CryptoJS.encrypt();
+//   console.log(encrypted);
+// }
